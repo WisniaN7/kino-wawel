9664709b5ca5ac94a3fab15171b8af7eb0cc3ccd
Jakub Wiśniewski <wisnian7@gmail.com>
2023-01-06 17:02:09 +0100

    Changed login button for users that are not signed in

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,26 @@ const urlParams = new URLSearchParams(queryString);
 const error = urlParams.get('status')
 const message = urlParams.get('message')
 
+
+window.getCookie = function (name) {
+    var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'))
+    if (match) return name == 'user' ? JSON.parse(match[2]) : match[2]
+}
+
+window.setCookie = function (name, value, days) {
+    var d = new Date
+    d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days)
+    document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString()
+}
+
+window.deleteCookie = function (name) {
+    document.cookie = name + "=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT"
+}
+
+if (getCookie('user')) {
+    setCookie('user', JSON.stringify(getCookie('user')), 7)
+}
+
 window.addEventListener('load', () => {
     const user = getCookie('user')
     let menu
@@ -16,23 +36,24 @@ window.addEventListener('load', () => {
             nav.insertBefore(li, nav.children[0])
         }
 
-        menu = createElementFromHTML('<menu> <li><p class="username">admin</p></li> <li><a href="#">Moje bilety</a></li> <li><a href="moje recenzje.html">Moje recenzje</a></li> <li><a href="obejrzane.html">Obejrzane filmy</a></li> <li><a href="#">Wyloguj</a></li> </menu>')
+        menu = createElementFromHTML('<div class="user"> <div class="icon-user"> <span class="head"></span> <span class="body"></span> </div> <menu> <li><p class="username">admin</p></li> <li><a href="moje bilety.html">Moje bilety</a></li> <li><a href="moje recenzje.html">Moje recenzje</a></li> <li><a href="obejrzane.html">Obejrzane filmy</a></li> <li><a href="#">Wyloguj</a></li> </menu></div>')
         
+        menu.querySelector('p.username').innerText = user.username
+
         menu.querySelector('li:last-child a').addEventListener('click', (e) => {
             e.preventDefault()
             deleteCookie('user')
-            window.location.href += '?status=10'
+            window.location.href = window.location.href.split('?')[0] + '?status=10'
         })
         
-    } else {
-        menu = createElementFromHTML('<menu> <li><a href="login.html">Zaloguj</a></li> <li><a href="rejestracja.html">Zarejestruj</a></li> </menu>')
+        const nav = document.querySelector('header nav')
+        nav.appendChild(menu)
+        nav.querySelector('li:last-child').remove()
+
+        document.querySelector('header div.user div.icon-user').addEventListener('click', function() {
+            document.querySelector('header div.user menu').classList.toggle('active')
+        })
     }
-
-    document.querySelector('header div.user').appendChild(menu)
-
-    document.querySelector('header div.user div.icon-user').addEventListener('click', function() {
-        document.querySelector('header div.user menu').classList.toggle('active')
-    })
 
     handleErrors()
 })
@@ -91,6 +112,10 @@ function handleErrors() {
         snackbar = createSnackbar('Dodanie nie powiodło się.', 'error', 'short')
     else if (error == 14)
         snackbar = createSnackbar('Aktualizacja nie powiodło się.', 'error', 'short')
+    else if (error == 15)
+        snackbar = createSnackbar('Dodanie filmu powiodło się.', 'success', 'short')
+    else if (error == 16)
+        snackbar = createSnackbar('Edycja filmu powiodła się.', 'success', 'short')
     else
         snackbar = createSnackbar('Unknown status: ' + error + ', with message: ' + message)
 }
@@ -107,19 +132,4 @@ function isAdmin(user) {
             return true
 
     return false
-}
-
-window.getCookie = function(name) {
-    var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'))
-    if (match) return name == 'user' ? JSON.parse(match[2]) : match[2]
-}
-
-window.setCookie = function(name, value, days) {
-    var d = new Date
-    d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days)
-    document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString()
-}
-
-window.deleteCookie = function(name) {
-    document.cookie = name + "=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT"
 }
