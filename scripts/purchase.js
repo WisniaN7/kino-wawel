c581f713e83a0df1f97efd5d88fdca9532057be8
Jakub Wiśniewski <wisniaN7@gmail.com>
2023-01-23 16:34:55 +0100

    Implemented discounts

--- scripts/purchase.js
+++ scripts/purchase.js
@@ -1,6 +1,13 @@
 // TODO: Check whether is it not too late to buy tickets
 // TODO: Discount codes
 
+function calcTotal() {
+    const tickets = document.querySelectorAll('.ticket-type .ticket-counter')
+    let total = 0
+    tickets.forEach(ticket => total += ticket.innerHTML * ticket.getAttribute('data-price') )
+    return total
+}
+
 const screeningId = urlParams.get('screeningId')
 
 const seats = fetch('https://wawel.herokuapp.com/movies/screening/' + screeningId)
@@ -51,14 +58,13 @@ const getSeats = async () => {
 }
 
 function addEventListeners() {
-    const total = document.querySelector('#summary span')
+    const totalDisplay = document.querySelector('#summary span')
     const ticketsSelector = document.querySelector('#tickets')
-    const countersPlus = ticketsSelector.querySelectorAll('.plus')
     const validateEvent = new Event('validate')
     const ticketValidator = ticketsSelector.querySelector('#validation input[type="radio"]')
-
+    
     document.querySelector('input[type="hidden"]').value = screeningId
-
+    
     ticketsSelector.addEventListener('validate', () => {
         if (ticketsSelector.getAttribute('data-required') == ticketsSelector.getAttribute('data-chosen') && ticketsSelector.getAttribute('data-chosen') != 0)
             ticketsSelector.classList.add('correct')
@@ -67,6 +73,8 @@ function addEventListeners() {
         else
             ticketsSelector.classList.remove('correct')
     })
+
+    const countersPlus = ticketsSelector.querySelectorAll('.plus')
 
     countersPlus.forEach((plus) => {
         plus.addEventListener('click', () => {
@@ -77,7 +85,8 @@ function addEventListeners() {
             const input = sibling.querySelector('input[type="number"]')
             input.value = parseInt(counter.innerText)
 
-            total.innerText = parseInt(total.innerText) + parseInt(counter.getAttribute('data-price'))
+            totalDisplay.innerText = calcTotal() * (100 - discount) / 100
+            
             ticketsSelector.setAttribute('data-required', parseInt(ticketsSelector.getAttribute('data-required')) + 1)
             ticketsSelector.classList.add('chosen')
             ticketsSelector.classList.remove('incorrect')
@@ -101,7 +110,8 @@ function addEventListeners() {
             const input = sibling.querySelector('input[type="number"]')
             input.value = parseInt(counter.innerText)
 
-            total.innerText = parseInt(total.innerText) - parseInt(counter.getAttribute('data-price'))
+            totalDisplay.innerText = calcTotal() * (100 - discount) / 100
+            
             ticketsSelector.setAttribute('data-required', parseInt(ticketsSelector.getAttribute('data-required')) - 1)
             ticketsSelector.classList.remove('incorrect')
             
@@ -124,13 +134,36 @@ function addEventListeners() {
                 ticketsSelector.setAttribute('data-chosen', parseInt(ticketsSelector.getAttribute('data-chosen')) - 1)
                 
                 if (parseInt(ticketsSelector.getAttribute('data-chosen')) == 0)
-                ticketsSelector.classList.remove('chosen')
+                    ticketsSelector.classList.remove('chosen')
             }
             
             ticketsSelector.classList.remove('incorrect')
             ticketsSelector.dispatchEvent(validateEvent)
         })
     })
+
+    const discountInput = document.querySelector('#inputs div input')
+    const discountBtn = document.querySelector('#inputs div button')
+    let discount = 0
+    
+    discountBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        discountBtn.classList.remove('correct')
+
+        if (!['WAWEL5', 'WAWEL10', 'WAWEL15'].includes(discountInput.value)) {
+            discountBtn.classList.add('error')
+            discountBtn.querySelector('span').innerText = '→'
+            return
+        } 
+
+        discount = parseInt(discountInput.value.split('WAWEL')[1])
+        totalDisplay.innerText = calcTotal() * (100 - discount) / 100
+
+        discountBtn.querySelector('span').innerText = '✔'
+        discountBtn.classList.add('correct')
+    })
+
+    discountInput.addEventListener('input', () => { discountBtn.classList.remove('error') })
 
     const submit = document.querySelector('button[type="submit"]')
 
