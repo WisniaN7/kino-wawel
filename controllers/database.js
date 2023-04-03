ab44452942b63bdbc6824278ad8e3af020572788
Jakub Wiśniewski <wisniaN7@gmail.com>
2023-04-03 11:49:11 +0200

    Environment variables

--- controllers/database.js
+++ controllers/database.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql2/promise')
 
+process.env.DB_URL = process.env.DB_URL || 'localhost'
+process.env.DB_PORT = process.env.DB_PORT || 3306
+process.env.DB_USER = process.env.DB_USER || 'root'
+process.env.DB_PASS = process.env.DB_PASS || ''
+process.env.DB_NAME = process.env.DB_NAME || 'kino_wawel'
+
 const createConnection = async () => {
     const conn = await mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: '',
-        database: 'kino_wawel'
+        host: process.env.DB_URL,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
     })
     
     await conn.connect();
